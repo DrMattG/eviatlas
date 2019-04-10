852db954ee930457a59e76f81e5bc2b5c7384557
Matthew Grainger <matthew.grainger@nina.no>
2019-04-10 15:06:21 +0200

    It's alive!!

--- Add_user_polygons/Add_user_polygons.R
+++ Add_user_polygons/Add_user_polygons.R
@@ -1,31 +1,30 @@
-#Norway PA to leaflet map
-require(sf)
-getwd()
-NorwayPA <- read_sf("C:/Users/matthew.grainger/OneDrive - NINA/Projects_in_development/EviAtlas/AddPolygons_leaflet/WDPA_Apr2019_NOR-shapefile/WDPA_Apr2019_NOR-shapefile-polygons.shp")
-NorwayPA
-leaflet() %>%
-    addTiles() %>% 
-  addPolygons(data = NorwayPA, fillColor = "green", fillOpacity = 0.5, color = "black", weight = 2)
+ui=shinyUI(fluidPage(
+     fileInput('shape', 'Select all files associated with the shapefile (.shp, .dbf,.sbn,.sbx,.shx and .prj)', multiple=TRUE, accept=c('.shp','.dbf','.sbn','.sbx','.shx',".prj")),
+     leafletOutput("mymap")
+   )
+ )
+ 
+ 
+ server = shinyServer(function(input,output){
 
-##I cant get the read_sf part to work through Rshiny - something to do with the path?
-# ui=shinyUI(fluidPage(
-#     fileInput('shape', 'Choose shapefile', multiple=FALSE, accept=c('.shp','.dbf','.sbn','.sbx','.shx',".prj")),
-#     leafletOutput("mymap")
-#   )
-# )
-# 
-# 
-# server = shinyServer(function(input,output){
-#  
-#  inShp = reactive({
-#    req(input$shape)
-#   sf::read_sf(input$shape$datafile)
-#   })
-#   
-#  output$mymap <- renderLeaflet({
-#       leaflet() %>%
-#      addTiles() %>% 
-#       addPolygons(data=inShp(), weight = 2, fillColor = "yellow")
-#     })
-# })
-# shinyApp(ui, server)
+   
+   data<-reactive({
+     req(input$shape)
+     shpdf <- input$shape
+     tempdirname <- dirname(shpdf$datapath[1])
+         for(i in 1:nrow(shpdf)){
+         file.rename(shpdf$datapath[i], paste0(tempdirname, "/", shpdf$name[i]))
+       }
+     data=readOGR(paste(tempdirname, shpdf$name[grep(pattern = "*.shp$", shpdf$name)], sep="/"))
+     
+   })
+  output$mymap <- renderLeaflet({
+    
+     leaflet() %>%
+         addTiles() %>% 
+       addPolygons(data = data(), fillColor = "green", fillOpacity = 0.5, color = "black", weight = 2)
+ })
+ })
+
+
+  shinyApp(ui, server)
